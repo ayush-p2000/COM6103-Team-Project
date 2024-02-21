f13e97d7a34ee403c25453e3b249c22ab572b0df
Benjamin Lister <48570319+007spy9@users.noreply.github.com>
2024-02-21 18:08:28 +0000

    Added Bootstrap Support

--- www/app.js
+++ www/app.js
@@ -19,6 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use("/stylesheets", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")))
+app.use("/javascripts", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")))
+app.use("/javascripts", express.static(path.join(__dirname, "node_modules/jquery/dist")))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
