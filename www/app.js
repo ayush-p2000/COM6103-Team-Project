4d9f82fa8732d559a18bcc96f578f070cd1626f5
Benjamin Lister <48570319+007spy9@users.noreply.github.com>
2024-02-22 16:25:35 +0000

    Added Reports View v1 Modified stylings slightly to include a light grey background and to use Roboto instead of Lucida Grande

--- www/app.js
+++ www/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var staffRouter = require('./routes/staff');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use("/javascripts", express.static(path.join(__dirname, "node_modules/jquery
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/staff', staffRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
