5d66e2c5ce40ea4d5368e5a9c637e68db49ecebb
Benjamin Lister <48570319+007spy9@users.noreply.github.com>
2024-02-22 18:10:34 +0000

    Started on individual reports

--- www/routes/staff.js
+++ www/routes/staff.js
@@ -6,4 +6,32 @@ router.get('/reports', function(req, res, next) {
     res.render('reports/reports', { title: 'Reports' });
 });
 
+router.get('/reports/sales', function(req, res, next) {
+    res.render('reports/report', { title: 'Sales', report: 'sales', data: {labels: [], datasets: []} });
+});
+
+router.get('/reports/referrals', function(req, res, next) {
+    res.render('reports/report', { title: 'Referrals', report: 'referrals', data: {labels: [], datasets: []} });
+});
+
+router.get('/reports/classes', function(req, res, next) {
+    res.render('reports/report', { title: 'Device Classes', report: 'device_classes', data: {labels: [], datasets: []} });
+});
+
+router.get('/reports/accounts', function(req, res, next) {
+    res.render('reports/report', { title: 'Accounts', report: 'accounts', data: {labels: [], datasets: []} });
+});
+
+router.get('/reports/account_types', function(req, res, next) {
+    res.render('reports/report', { title: 'Account Types', report: 'account_types', data: {labels: [], datasets: []} });
+});
+
+router.get('/reports/cases', function(req, res, next) {
+    res.render('reports/report', { title: 'Cases', report: 'cases', data: {labels: [], datasets: []} });
+});
+
+router.get('/reports/devices', function(req, res, next) {
+    res.render('reports/report', { title: 'Devices', report: 'devices', data: {labels: [], datasets: []} });
+});
+
 module.exports = router;
