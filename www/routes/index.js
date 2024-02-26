5818b17bc6675e6e98d5736178beb95673496cc6
Vinroy2308 <vinroy2308@gmail.com>
2024-02-26 17:30:15 +0000

    Removed redundant navlinks and added hrefs

--- www/routes/index.js
+++ www/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // auth and role variables used to change the navbar links
+  //TODO: auth and role variables used to change the navbar links.
   res.render('index', { title: 'Express', auth:true, role:'user'});
 });
 
