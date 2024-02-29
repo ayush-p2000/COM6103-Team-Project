ddb3caf7c43dcada697976226a82d06dba4bd5e8
Ayush Prajapati <92866992+ayush-p2000@users.noreply.github.com>
2024-02-29 06:19:39 +0000

    TSP-73 | updated version
    Removed sidebars, added widget buttons. Did a small styling on navbar (will work fine in every page). Tested for responsiveness (its good/enough). Removed the not required css. The added css has a self made class name, so it'll not conflict with other classes.
    
    The landing page has not added yet, so I put the background theme same as the navbar color.

--- www/routes/user.js
+++ www/routes/user.js
@@ -166,7 +166,7 @@ router.get("/user_dashboard", (req,res,next) => {
             owner: "Ayush"
         },
     ]
-    res.render('user_dashboard', {items})
+    res.render('user_dashboard_2', {items, auth: true, role: 'user'})
 })
 
 
