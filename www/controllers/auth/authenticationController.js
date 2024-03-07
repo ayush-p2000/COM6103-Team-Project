0f484ebba35953d8a94252031617f97a24fc82b1
xenono <aurbanczyk1@sheffield.ac.uk>
2024-03-07 15:18:39 +0000

    Fix paths to views.

--- www/controllers/auth/authenticationController.js
+++ www/controllers/auth/authenticationController.js
@@ -4,12 +4,12 @@
 
 function getLoginPage(req, res, next) {
     //TODO: Add functionality for the login page
-    res.render("auth/login", {auth: false})
+    res.render("authentication/login", {auth: false})
 }
 
 function getRegisterPage(req, res, next) {
     //TODO: Add functionality for the register page
-    res.render("auth/register", {auth: false})
+    res.render("authentication/register", {auth: false})
 }
 
 module.exports = {
