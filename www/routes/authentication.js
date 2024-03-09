a75b7bf63d9431ff3fc12cf82ee219279ba723d6
xenono <aurbanczyk1@sheffield.ac.uk>
2024-03-09 18:15:30 +0000

    Add missing fields to the registration form, add registering and authenticating users. Assign auth middleware to restricted routes.

--- www/routes/authentication.js
+++ www/routes/authentication.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const {getLoginPage, getRegisterPage} = require("../controllers/auth/authenticationController");
+const { passportAuthenticate } = require("../auth/passportAuth")
+const {getLoginPage, getRegisterPage, registerUser ,logoutUser} = require("../controllers/auth/authenticationController");
+const {isAuthenticated} = require("../middlewares/auth");
 
 router.get('/login', getLoginPage);
+router.post("/login",passportAuthenticate)
+router.post("/logout", isAuthenticated, logoutUser)
 
 router.get('/register', getRegisterPage );
+router.post('/register', registerUser)
 
 module.exports = router;
