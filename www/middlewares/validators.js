ed692c0af1fd1506548bcfbbd8796cd1a58a002b
xenono <aurbanczyk1@sheffield.ac.uk>
2024-03-09 23:56:10 +0000

    Add validator middleware for santisation and validation forms on the login and registration page.

---
+++ www/middlewares/validators.js
@@ -0,0 +1,24 @@
+const {body,check, validationResult} = require("express-validator")
+
+const errorsToSession = (req, res, next) => {
+    const {errors} = validationResult(req)
+    console.log(errors)
+    req.session.messages = errors.map(error => error.msg)
+    next()
+}
+
+exports.validateRegistration = [
+    check("firstName","Incorrect First Name").trim().escape().notEmpty().isAlpha(),
+    check("lastName","Incorrect Last Name").isAlpha().trim().escape().notEmpty(),
+    check("email","Invalid email address").trim().normalizeEmail().isEmail().notEmpty(),
+    check("dateOfBirth","Invalid Date Of Birth").trim().escape().isDate({format:"YYYY-MM-DD"}),
+    check("password","Password must be at least 4 characters long").trim().escape().notEmpty().isLength({min: 4}),
+    check("confirmPassword","Passwords do not match.").trim().escape().notEmpty().custom((value, {req}) => value === req.body.password),
+    errorsToSession
+]
+
+exports.validateLogin = [
+    body("email").trim().normalizeEmail().isEmail().notEmpty(),
+    body("password").trim().escape()
+]
+
--- www/routes/authentication.js
+++ www/routes/authentication.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-const { passportAuthenticate } = require("../auth/passportAuth")
-const {getLoginPage, getRegisterPage, registerUser ,logoutUser} = require("../controllers/auth/authenticationController");
+const {passportAuthenticate} = require("../auth/passportAuth")
+const {
+    getLoginPage,
+    getRegisterPage,
+    registerUser,
+    logoutUser
+} = require("../controllers/auth/authenticationController");
+const {validateRegistration, validateLogin} = require("../middlewares/validators")
 const {isAuthenticated} = require("../middlewares/auth");
+const { body } = require("express-validator")
 
 router.get('/login', getLoginPage);
-router.post("/login",passportAuthenticate)
+router.post("/login", validateLogin, passportAuthenticate)
 router.post("/logout", isAuthenticated, logoutUser)
 
-router.get('/register', getRegisterPage );
-router.post('/register', registerUser)
+router.get('/register', getRegisterPage);
+const sanitizer = body('firstName').notEmpty().trim().escape()
+router.post('/register', validateRegistration,registerUser)
 
 module.exports = router;
