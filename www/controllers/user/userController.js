46073a071bfda18ba69b5142f7265c1df4fff300
Koustav Muhuri <kmuhuri1@sheffield.ac.uk>
2024-03-11 17:35:29 +0000

    TSP-33 || Create Staff Account

--- www/controllers/user/userController.js
+++ www/controllers/user/userController.js
@@ -3,10 +3,42 @@
  */
 
 const {getMockUser} = require("../../util/mock/mockData");
+const {User} = require("../../model/schema/user");
+const createUser = require("../../model/mongodb")
+const crypto = require('crypto');
+const {randomBytes, pbkdf2} = require("node:crypto")
 
 function getUserDashboard(req, res, next) {
     //TODO: Add functionality for the user dashboard
     res.render("user/dashboard", {user: getMockUser(), auth: true, role: "user"})
+}
+
+async function insertStaffDetails(req,res,next){
+
+    const salt = crypto.randomBytes(12).toString('hex');
+    const address ={
+        address_1:req.body.addressFirst,
+        address_2:req.body.addressSecond,
+        postcode:req.body.postCode,
+        country:req.body.country,
+        county: "Manhattan",
+        city:req.body.city
+    };
+
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password,
+        first_name: req.body.firstName,
+        last_name: req.body.lastName,
+        address: address,
+        phone_number: req.body.phone,
+        salt:salt
+    });
+
+    await createUser.createUser(user);
+
+    console.log(req.body);
+    res.render("user/dashboard",{user:getMockUser(), auth:true, role:"user"})
 }
 
 function getUserProfile(req, res, next) {
@@ -16,5 +48,6 @@ function getUserProfile(req, res, next) {
 
 module.exports = {
     getUserProfile,
-    getUserDashboard
+    getUserDashboard,
+    insertStaffDetails
 }
--- www/routes/user.js
+++ www/routes/user.js
@@ -2,10 +2,12 @@ const express = require('express');
 const {faker} = require("@faker-js/faker");
 const router = express.Router();
 
-const {getUserProfile, getUserDashboard} = require("../controllers/user/userController");
+const {getUserProfile, getUserDashboard,insertStaffDetails} = require("../controllers/user/userController");
 router.get('/profile', getUserProfile)
 
 router.get('/dashboard', getUserDashboard);
+
+router.post('/registration',insertStaffDetails)
 
 
 module.exports = router;
