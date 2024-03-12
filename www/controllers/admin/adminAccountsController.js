b67dddb3ef1d4b4323ca64a900b99ccd99413361
Benjamin Lister <48570319+007spy9@users.noreply.github.com>
2024-03-12 16:25:04 +0000

    TSP-39 | Added basic user access for admins

--- www/controllers/admin/adminAccountsController.js
+++ www/controllers/admin/adminAccountsController.js
@@ -4,15 +4,21 @@
 
 const {getMockAccountsList, getMockUser} = require('../../util/mock/mockData')
 const {renderAdminLayout, renderAdminLayoutPlaceholder} = require("../../util/layout/layoutUtils");
+const {getAllUsers, getUserById, searchUserAndPopulate} = require("../../model/mongodb");
 
-function getAccountsPage(req, res, next) {
-    const users = getMockAccountsList();
-    renderAdminLayout(req, res, "user_management", {users});
+async function getAccountsPage(req, res, next) {
+    let users = [];
+    try {
+        users = await getAllUsers();
+        renderAdminLayout(req, res, "user_management", {users});
+    } catch (e) {
+        console.error(e);
+        renderAdminLayout(req, res, "user_management", {users, error: "Failed to retrieve user data"});
+    }
 }
 
-function getAccountDetailsPage(req, res, next) {
-// TODO Remove dummy retrieval
-    const user = getMockUser()
+async function getAccountDetailsPage(req, res, next) {
+    const user = await searchUserAndPopulate({_id: req.params.id});
     renderAdminLayout(req, res, "user_details", {user});
 }
 
