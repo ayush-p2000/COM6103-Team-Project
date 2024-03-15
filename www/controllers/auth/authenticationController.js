f1801188c7d888bf6ea459968ce053bbf8fe3ee7
Ayush Prajapati <92866992+ayush-p2000@users.noreply.github.com>
2024-03-15 16:37:51 +0000

    TSP-94
    Email match handling added

--- www/controllers/auth/authenticationController.js
+++ www/controllers/auth/authenticationController.js
@@ -12,8 +12,22 @@ let user = ""
 //------------------------------------------------- User Registration Data feeding and Authenticating -------------------------------------------//
 
 const registerUser = async (req, res, next) => {
+    let messages;
     try {
-        const {firstName, lastName, dateOfBirth, email, password, phone, address_1, address_2, postcode, city, county, country} = req.body
+        const {
+            firstName,
+            lastName,
+            dateOfBirth,
+            email,
+            password,
+            phone,
+            address_1,
+            address_2,
+            postcode,
+            city,
+            county,
+            country
+        } = req.body
 
         // These fields should be included in the form and validated appropriately
         // Refer to the bug - TSP-94
@@ -31,12 +45,11 @@ const registerUser = async (req, res, next) => {
 
         const salt = randomBytes(16)
         const hashedPassword = await pbkdf2Promise(password, salt, 310000, 32, 'sha256')
-        const emailCheck = await User.findOne({ email });
-        if(emailCheck) {
-            return res.send('email already exists') // needs refined alert
-        }
-        else
-        {
+        const emailCheck = await User.findOne({email});
+        if (emailCheck) {
+            messages = ['User email already exists']
+            return res.render("authentication/register", {messages: messages, hasMessages: messages.length>0, auth: req.isLoggedIn, user:req.user}) // needs refined alert
+        } else {
             user = new User({
                 first_name: firstName,
                 last_name: lastName,
@@ -48,7 +61,7 @@ const registerUser = async (req, res, next) => {
                 address
             });
         }
-        if(req.session.messages.length > 0){
+        if (req.session.messages.length > 0) {
             return res.redirect("/register")
         }
 
