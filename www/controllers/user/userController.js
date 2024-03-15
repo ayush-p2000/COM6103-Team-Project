72b82b5ee72f89bf460b8b54240e99a6fd23e987
Ayush Prajapati <92866992+ayush-p2000@users.noreply.github.com>
2024-03-15 15:23:13 +0000

    TSP-79
    User profile details value showing up on profile.

--- www/controllers/user/userController.js
+++ www/controllers/user/userController.js
@@ -2,18 +2,43 @@
  * This controller should handle any operations related to user dashboard and miscellaneous user operations
  */
 
-const {getMockUser} = require("../../util/mock/mockData");
+//const {getMockUser} = require("../../util/mock/mockData");
+const {User} = require("../../model/schema/user");
 
-function getUserDashboard(req, res, next) {
-    res.render("user/dashboard", {user: req.user , auth: req.isLoggedIn})
+async function getUserDashboard(req, res, next) {
+    const userData = await User.findById({_id: req.user.id});
+    res.render("user/dashboard", {user: userData, auth: req.isLoggedIn})
 }
 
-function getUserProfile(req, res, next) {
+async function getUserProfile(req, res, next) {
     // TODO Fetch Full data of the user from the database
-    res.render("user/user_profile", {user: req.user , auth: req.isLoggedIn})
+    try {
+        const userData = await User.findById({_id: req.user.id});
+        console.log(userData)
+        res.render("user/user_profile", {user: userData, auth: req.isLoggedIn})
+
+    } catch (err) {
+        res.send('No user found')
+    }
+
+}
+
+async function updateUserDetails(req, res, next){
+    try {
+        const user = await User.findById({_id: req.user.id});
+        user.first_name = req.body.firstName || user.first_name
+
+        user.save();
+
+        res.render("user/user_profile", {user: user, auth: req.isLoggedIn})
+    }
+    catch (err){
+        res.send(err)
+    }
 }
 
 module.exports = {
     getUserProfile,
-    getUserDashboard
+    getUserDashboard,
+    updateUserDetails
 }
