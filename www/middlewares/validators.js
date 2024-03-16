366c27abb95d0e3bf2ca0d47bdefd571ee7ef7f4
Ayush Prajapati <92866992+ayush-p2000@users.noreply.github.com>
2024-03-16 23:40:52 +0000

    TSP-94
    Added validation for phone. Please add by yourself if anything seem missing.

--- www/middlewares/validators.js
+++ www/middlewares/validators.js
@@ -16,6 +16,10 @@ const checkAge = (value, {req}) => {
     return yearDiff >= 13
 }
 
+const checkPhoneNumber = (value) => {
+    return /^\d{10}$/.test(value); // Check if value is a 10-digit number
+};
+
 exports.validateRegistration = [
     check("firstName","Incorrect First Name").trim().escape().notEmpty().isAlpha(),
     check("lastName","Incorrect Last Name").isAlpha().trim().escape().notEmpty(),
@@ -23,6 +27,7 @@ exports.validateRegistration = [
     check("dateOfBirth","Invalid Date Of Birth").trim().escape().isDate({format:"YYYY-MM-DD"}).custom(checkAge).withMessage("You need to be at least 13 years old"),
     check("password","Password must be at least 8 characters long").trim().escape().notEmpty().isLength({min: 8}),
     check("confirmPassword","Passwords do not match.").trim().escape().notEmpty().custom((value, {req}) => value === req.body.password),
+    check("phoneNumber", "Invalid number").trim().escape().custom(checkPhoneNumber),
     errorsToSession
 ]
 
