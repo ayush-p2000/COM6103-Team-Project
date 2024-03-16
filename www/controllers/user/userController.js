f5e67e967177d9f01e79ab25b96c3d63094da65b
Ayush Prajapati <92866992+ayush-p2000@users.noreply.github.com>
2024-03-16 16:09:37 +0000

    TSP - 79
    User profile editing has been implemented.

--- www/controllers/user/userController.js
+++ www/controllers/user/userController.js
@@ -5,10 +5,15 @@
 //const {getMockUser} = require("../../util/mock/mockData");
 const {User} = require("../../model/schema/user");
 
+//------------------------------------------------ Rendering user Database -------------------------------------------------------------------------//
+
+
 async function getUserDashboard(req, res, next) {
     const userData = await User.findById({_id: req.user.id});
     res.render("user/dashboard", {user: userData, auth: req.isLoggedIn})
 }
+
+//------------------------------------------------ Get User Data from Database -------------------------------------------------------------------------//
 
 async function getUserProfile(req, res, next) {
     // TODO Fetch Full data of the user from the database
@@ -23,17 +28,73 @@ async function getUserProfile(req, res, next) {
 
 }
 
+//------------------------------------------------ Get User Data from Database -------------------------------------------------------------------------//
+
+
 async function updateUserDetails(req, res, next){
     try {
-        const user = await User.findById({_id: req.user.id});
-        user.first_name = req.body.firstName || user.first_name
+        const { firstName, lastName, phone, addressFirst, addressSecond, postCode, city, county, country } = req.body; // Assuming these fields can be updated
 
-        user.save();
+        // Construct an object with the fields that need to be updated
+        const updateFields = {};
 
-        res.render("user/user_profile", {user: user, auth: req.isLoggedIn})
+        if (firstName) updateFields.first_name = firstName; //update first name
+
+        if (lastName) updateFields.last_name = lastName; //update last name
+
+        if (phone) updateFields.phone_number = phone; //update phone
+
+        //update address line 1
+        if (addressFirst) {
+            updateFields.address = updateFields.address || {};
+            updateFields.address.address_1 = addressFirst;
+        }
+
+        //update address line 2
+        if (addressSecond) {
+            updateFields.address = updateFields.address || {};
+            updateFields.address.address_2 = addressSecond;
+        }
+
+        //update city
+        if (city) {
+            updateFields.address = updateFields.address || {};
+            updateFields.address.city = city;
+        }
+
+        //update postcode
+        if (postCode) {
+            updateFields.address = updateFields.address || {};
+            updateFields.address.postcode = postCode;
+        }
+
+        //update county
+        if (county) {
+            updateFields.address = updateFields.address || {};
+            updateFields.address.county = county;
+        }
+
+        //update county
+        if (country) {
+            updateFields.address = updateFields.address || {};
+            updateFields.address.country = country;
+        }
+
+
+
+        // Find the user by ID and update the specified fields
+        const updatedUser = await User.findByIdAndUpdate(req.user.id, updateFields, { new: true });
+
+        if (!updatedUser) {
+            return res.status(404).send('User not found');
+        }
+
+        res.render("user/user_profile", { user: updatedUser, auth: req.isLoggedIn });
     }
-    catch (err){
-        res.send(err)
+    // catching error
+    catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
     }
 }
 
