b77f4c2ddf17d6dd14d96d50a9cf77cc968c37a3
Koustav Muhuri <kmuhuri1@sheffield.ac.uk>
2024-03-25 16:51:04 +0000

    TSP-33 || Create Staff Account

--- www/controllers/user/userController.js
+++ www/controllers/user/userController.js
@@ -4,6 +4,13 @@
 
 //const {getMockUser} = require("../../util/mock/mockData");
 const {User} = require("../../model/schema/user");
+const {randomBytes, pbkdf2} = require("node:crypto")
+const {promisify} = require("node:util");
+const pbkdf2Promise = promisify(pbkdf2)
+const {getAllUsers} = require("../../model/mongodb");
+const {renderAdminLayout} = require("../../util/layout/layoutUtils");
+const {USER} = require("../../model/enum/roleTypes");
+let user = ""
 
 //------------------------------------------------ Rendering user Database -------------------------------------------------------------------------//
 
@@ -105,8 +112,76 @@ async function updateUserDetails(req, res, next){
     }
 }
 
+const registerUser = async (req, res, next) => {
+    let messages;
+    req.body;
+    try {
+        const {
+            firstName,
+            lastName,
+            dateOfBirth,
+            email,
+            password,
+            phone,
+            address_1,
+            address_2,
+            postcode,
+            city,
+            county,
+            country,
+            role
+
+        } = req.body
+
+        // These fields should be included in the form and validated appropriately
+        // Refer to the bug - TSP-94
+        const phone_number = phone;
+
+        const address = {
+            address_1: address_1,
+            address_2: address_2,
+            city: city,
+            county: county,
+            country: country,
+            postcode: postcode
+        }
+
+
+        const salt = randomBytes(16)
+        const hashedPassword = await pbkdf2Promise(password, salt, 310000, 32, 'sha256')
+        const emailCheck = await User.findOne({email});
+        if (emailCheck) {
+            messages = ['User email already exists']
+        } else {
+            user = new User({
+                first_name: firstName,
+                last_name: lastName,
+                date_of_birth: dateOfBirth,
+                phone_number,
+                email,
+                password: hashedPassword,
+                salt,
+                address,
+                role:role
+            });
+        }
+        if(req.user.role > USER) {
+            await user.save()
+        }
+
+        let users = [];
+        users = await getAllUsers();
+        renderAdminLayout(req, res, "user_management", {users});
+
+
+    } catch (err) {
+        return next(err)
+    }
+}
+
 module.exports = {
     getUserProfile,
     getUserDashboard,
-    updateUserDetails
+    updateUserDetails,
+    registerUser
 }
--- www/routes/user.js
+++ www/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {faker} = require("@faker-js/faker");
 const router = express.Router();
 
-const {getUserProfile, getUserDashboard, updateUserDetails} = require("../controllers/user/userController");
+const {getUserProfile, getUserDashboard, updateUserDetails, registerUser} = require("../controllers/user/userController");
 const {getMyItems} = require("../controllers/marketplace/marketplaceController");
 
 router.get('/profile', getUserProfile)
@@ -12,5 +12,7 @@ router.post('/profile', updateUserDetails)
 router.get('/dashboard', getUserDashboard);
 
 router.get('/my-items/:page?', getMyItems);
+
+router.post('/addUser', registerUser);
 
 module.exports = router;
