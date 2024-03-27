0e47d9553fb16c103835389c4e600e805775fc2d
xenono <aurbanczyk1@sheffield.ac.uk>
2024-03-27 22:17:25 +0100

    Bring back correct secret key.

--- www/middlewares/captcha.js
+++ www/middlewares/captcha.js
@@ -6,7 +6,7 @@
 const axios = require("axios");
 
 exports.validateCaptcha = async (req, res, next) => {
-    const secret_key = process.env.CAPTCHA_SECRET + "x"
+    const secret_key = process.env.CAPTCHA_SECRET
     const token = req.body.token;
     const captchaApi = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token}`;
     const captcha = await axios.post(captchaApi)
