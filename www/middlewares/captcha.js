16bdc99da509c683814eca040f2b4e26029f537b
xenono <aurbanczyk1@sheffield.ac.uk>
2024-03-28 23:13:30 +0100

    Fix captcha form being submited before captcha check had finished causing undefined captcha behaviour.

--- www/middlewares/captcha.js
+++ www/middlewares/captcha.js
@@ -6,14 +6,20 @@
 const axios = require("axios");
 
 exports.validateCaptcha = async (req, res, next) => {
+    if(req.body.token === undefined || req.body.token === '' || req.body.token === null)
+    {
+        console.log(req.body)
+        return res.json({"responseError" : "something goes to wrong"});
+    }
     const secret_key = process.env.CAPTCHA_SECRET;
     const token = req.body.token;
-    const captchaApi = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token}`;
-    const captcha = await axios.post(captchaApi)
+    const captchaApi = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token}&remoteip=${req.connection.remoteAddress}`
 
+    const captcha = await axios.post(captchaApi)
     if (captcha && captcha.data.success) {
         return next()
     }
+
     req.session.messages = ["Google ReCaptchaV3 Check Failed!","Please restart your browser or your computer."]
     return res.redirect(req.path)
 }
--- www/public/javascripts/captcha/captcha.js
+++ www/public/javascripts/captcha/captcha.js
@@ -1,12 +1,21 @@
 const form = document.getElementById("form")
+const submitBtn = document.getElementById("submit-btn")
+let isCaptchaCheckFinished = false;
+form.addEventListener('submit', e => {
+    // Prevent submitting the form before the captcha check had finished.
+    if(!isCaptchaCheckFinished){
+        e.preventDefault()
+    }
+})
+const executeCaptcha = (publicKey, actionName, hasFailedCaptcha) => {
+    if(hasFailedCaptcha) return;
 
-const executeCaptcha = (publicKey, actionName) => {
     grecaptcha.ready(function () {
         grecaptcha.execute(publicKey, {action: actionName})
             .then(function (token) {
-                form.addEventListener('submit', () => {
-                    document.getElementById("token").value = token
-                })
+                isCaptchaCheckFinished = true
+                submitBtn.disabled = false
+                document.getElementById("token").value = token
             });
     });
 }
