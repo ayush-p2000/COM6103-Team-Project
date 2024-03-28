e43244e5dc979686b572bf67220f86bfef0af504
xenono <aurbanczyk1@sheffield.ac.uk>
2024-03-28 23:14:34 +0100

    Remove console logs and redundant error checks

--- www/middlewares/captcha.js
+++ www/middlewares/captcha.js
@@ -6,11 +6,6 @@
 const axios = require("axios");
 
 exports.validateCaptcha = async (req, res, next) => {
-    if(req.body.token === undefined || req.body.token === '' || req.body.token === null)
-    {
-        console.log(req.body)
-        return res.json({"responseError" : "something goes to wrong"});
-    }
     const secret_key = process.env.CAPTCHA_SECRET;
     const token = req.body.token;
     const captchaApi = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token}&remoteip=${req.connection.remoteAddress}`
