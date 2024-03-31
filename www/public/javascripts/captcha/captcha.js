d0e1caf29cb78741c1c608502cc9feef94d5983e
xenono <aurbanczyk1@sheffield.ac.uk>
2024-03-31 22:02:07 +0200

    Add loading spinners to the login and register forms.

--- www/public/javascripts/captcha/captcha.js
+++ www/public/javascripts/captcha/captcha.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("form")
 const submitBtn = document.getElementById("submit-btn")
+const spinner = document.getElementById("spinner")
 let isCaptchaCheckFinished = false;
 form.addEventListener('submit', e => {
     // Prevent submitting the form before the captcha check had finished.
@@ -15,6 +16,7 @@ const executeCaptcha = (publicKey, actionName, hasFailedCaptcha) => {
             .then(function (token) {
                 isCaptchaCheckFinished = true
                 submitBtn.disabled = false
+                submitBtn.textContent = actionName
                 document.getElementById("token").value = token
             });
     });
