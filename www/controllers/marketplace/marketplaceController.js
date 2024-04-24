bc77cb78af7223816fa0d3e3702dfc452dbc0ccf
xenono <aurbanczyk1@sheffield.ac.uk>
2024-04-24 19:38:05 +0100

    Utilise utils function for handling missing models

--- www/controllers/marketplace/marketplaceController.js
+++ www/controllers/marketplace/marketplaceController.js
@@ -30,33 +30,7 @@ const getMarketplace = async (req, res, next) => {
         let {items, pagination} = await getPaginatedResults(Device, req.params.page, {}, {}, 10);
 
         for (const item of items) {
-            if (item.model == null) {
-                var deviceType = ""
-                var brand = ""
-                var model = ""
-
-                const customModel = await getUnknownDeviceHistoryByDevice(item._id)
-                if (customModel.length > 0 && customModel[0].data) {
-                    customModel[0].data.forEach(data => {
-                        if (data.name === "device_type") {
-                            deviceType = data.value
-                        } else if (data.name === "brand") {
-                            brand = data.value
-                        } else if (data.name === "model") {
-                            model = data.value
-                        }
-                    });
-                } else {
-                    deviceType = "MISSING TYPE"
-                    brand = "MISSING BRAND"
-                    model = "MISSING MODEL"
-                }
-                item.device_type = {name: deviceType}
-                item.brand = {name: brand}
-                item.model = {name: model}
-            }
-
-
+            await handleMissingModels(item)
         }
         renderUserLayout(req, res, '../marketplace/marketplace', {
             deviceTypes,
